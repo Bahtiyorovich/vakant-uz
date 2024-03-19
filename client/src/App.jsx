eb889f46827552a24ed1efe204d9ddc0af54c453
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2024-03-19 17:30:18 +0500

    component

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Client, Footer, Main, Navbar, Products } from "./components"
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Main/>
+      <Client/>
+      <Products/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
